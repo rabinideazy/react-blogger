78f53d5554ebb4ce883a89b46733e4323524b61a
rabinideazy <rabin.ideazy@gmail.com>
2022-03-05 09:27:29 +0530

    added blog api

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+import Constants from '../../modules/config';
 import * as services from '../../modules/services';
+
+import './blog.scss';
 
 export default class Blog extends Component {
   constructor( props ) {
     super( props );
 
     this.state = {
-      loggedIn: services.loggedIn() ? true : false
+      loggedIn: services.loggedIn() ? true : false,
+      blogs: []
     }
+  }
+
+  componentDidMount() {
+    axios.get(`${Constants.endpoint}/blogs`).then((res) => {
+      this.setState({blogs: res.data});
+    })
+    .catch((error) => {
+      console.log(error.response.data.data);
+    });
+  }
+
+  createMarkup(content) {
+    return {__html: content};
   }
   
   render() {
@@ -18,7 +36,16 @@ export default class Blog extends Component {
 
     return (
       <div>
-        This is blog..
+        {
+          this.state.blogs.map((d,i) => {
+            return (
+              <div className="blog" key={i}>
+                <div className="blog-img"><img src={d.thumbnail_url} alt="Blog Image" /></div>
+                <div className="blog-content" dangerouslySetInnerHTML={this.createMarkup(d.post_content)}></div>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
