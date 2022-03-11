e627028af6d576058748ebf9299ffa3dc1d71ab1
Rabin Ray <46760139+rabinideazy@users.noreply.github.com>
2022-03-11 10:47:00 +0530

    Update config.js

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,4 +1,4 @@
 const Constatnts = {
-  endpoint: 'http://testsspace.com/projects2022/react-blogger/wp-json/vcap-services/v1'
+  endpoint: 'https://testsspace.com/projects2022/react-blogger/wp-json/vcap-services/v1'
 }
 export default Constatnts;
